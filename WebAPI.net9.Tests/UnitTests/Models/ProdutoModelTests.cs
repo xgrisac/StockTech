ec4e6cd71c3f7b8126b257affd59959386e6f466
Isac Ribeiro <isackolling122@gmail.com>
2025-09-02 18:27:42 -0300

    Reorganizando estrutura para incluir sln e tests na raiz

---
+++ WebAPI.net9.Tests/UnitTests/Models/ProdutoModelTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.net9.Models;
+using Xunit;
+
+namespace WebAPI.net9.Tests.UnitTests.Models
+{
+    public class ProdutoModelTests
+    {
+        [Fact]
+        public void QuantidadeEstoque_NaoPodeSerNegativa()
+        {
+            // Arrange
+            var produto = new ProdutoModel
+            {
+                QuantidadeEstoque = -5, // valor inválido
+            };
+
+            // Act
+            bool estoqueValido = produto.QuantidadeEstoque >= 0;
+
+            // Assert
+            estoqueValido.Should().BeFalse("A quantidade de estoque não pode ser negativa");
+        }
+    }
+}
